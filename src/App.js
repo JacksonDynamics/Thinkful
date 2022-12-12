732adf87f464ccfe1e0527df3e28a2b8598019e1
studmanj <bjackso3@hotmail.com>
2022-12-11 22:20:02 -0700

    Async State

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 
 function App() {
-
   const [subscribed, setSubscribed] = useState(false);
+  const [subscribedCount, setSubscribedCount] = useState(0);
   const [alerts, setAlerts] = useState(false);
 
   return (
     <section>
       <p>Please click to subscribe to my updates!</p>
-      <button onClick={() => setSubscribed(!subscribed)}>
+      <p>Subscriber Count: {subscribedCount}</p>
+      <button
+        onClick={() => {
+          setSubscribed(!subscribed);
+          setSubscribedCount(subscribedCount + 1);
+          if (!alerts) setAlerts(true);
+        }}
+      >
         {subscribed ? "Unsubscribe" : "Subscribe"}
       </button>
       <button onClick={() => setAlerts(!alerts)}>
         {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
       </button>
     </section>
-
   );
 }
 
