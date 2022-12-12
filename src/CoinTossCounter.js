52cb3a2f32051eecae54cbb0cfc45824a6c9780f
studmanj <bjackso3@hotmail.com>
2022-12-12 15:55:19 -0700

    Object

--- src/CoinTossCounter.js
+++ src/CoinTossCounter.js
@@ -3,17 +3,29 @@ import React, { useState } from "react";
 
 function CoinTossCounter() {
   const [results, setResults] = useState([]);
+  const [counts, setCounts] = useState({ H: 0, T: 0 });
 
-  const handleClick = (value) => setResults([...results, value]);
 
-  const handleDeleteLast = () => {
-    const filteredList = results.filter((result, index) => index !== results.length - 1);
-    setResults(filteredList);
+  const handleClick = (value) => {
+    setResults([...results, value]);
+    setCounts({
+      ...counts,
+      [value]: counts[value] + 1,
+    });
   };
-
-  const lis = results.map((result, index) => (
-    <li key={`toss-${index}`}>{result === "H" ? "Heads" : "Tails"}</li>
-  ));
+  
+  const handleDeleteLast = () => {
+    const last = results[results.length - 1];
+    const list = results.slice(0, -1);
+  
+    setResults(list);
+    if (last) {
+      setCounts({
+        ...counts,
+        [last]: counts[last] - 1,
+      });
+    }
+  };
 
   return (
     <section>
@@ -22,7 +34,10 @@ function CoinTossCounter() {
         <button onClick={() => handleClick("T")}>Tails</button>
         <button onClick={handleDeleteLast}>Delete Last</button>
       </div>
-      <ul>{lis}</ul>
+      <ul>
+        <li>Heads: {counts["H"]}</li>
+        <li>Tails: {counts["T"]}</li>
+      </ul>
     </section>
   );
 }
