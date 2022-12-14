11c5654445abb5f426bdf915202ce5e53976fd54
studmanj <bjackso3@hotmail.com>
2022-12-13 18:50:53 -0700

    Subscribers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import SubscriberForm from './SubscriberForm'
+import { useState } from "react";
+import SubscriberForm from "./SubscriberForm";
+import SubscriberList from "./SubscriberList";
+import SubscriberView from "./SubscriberView";
 
 
 function App() {
-  return <SubscriberForm />;
+  const [subscribers, setSubscribers] = useState([]);
+
+  const createSubscriber = (newSubscriber) =>
+    setSubscribers((currentSubscribers) => [
+      newSubscriber,
+      ...currentSubscribers,
+    ]);
+
+  const deleteSubscriber = (indexToDelete) =>
+    setSubscribers((currentSubscribers) =>
+      currentSubscribers.filter((post, index) => index !== indexToDelete)
+    );
+
+  return (
+    <>
+      <SubscriberForm createSubscriber={createSubscriber} />
+      <SubscriberList
+        subscribers={subscribers}
+        deleteSubscriber={deleteSubscriber}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/SubscriberForm.js
+++ src/SubscriberForm.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 
 
-function SubscriberForm() {
+function SubscriberForm({ createSubscriber }) {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const handleNameChange = (event) => setName(event.target.value);
+  const handleEmailChange = (event) => setEmail(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted:", name, email);
+    createSubscriber({name, email});
+    setName("");
+    setEmail("");
+  };
 
   console.log("Current value of name:", name);
+  console.log("Current value of email:", email);
   return (
-    <form>
+    <form onSubmit={handleSubmit} >
       <label htmlFor="name">
         Enter Your Name:
         <input
@@ -20,8 +31,16 @@ function SubscriberForm() {
       </label>
       <label htmlFor="email">
         Your Email:
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleEmailChange}
+          value={email}
+        />
       </label>
+      <button type="submit">Submit</button>
+
     </form>
   );
 }
