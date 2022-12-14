5fae6320e0834893645d68f9a5b5dc314f8b9618
studmanj <bjackso3@hotmail.com>
2022-12-13 22:50:48 -0700

    Complex Form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import { useState } from "react";
 import SubscriberForm from "./SubscriberForm";
-import SubscriberList from "./SubscriberList";
 
 
 function App() {
-  const [subscribers, setSubscribers] = useState([]);
-
-  const createSubscriber = (newSubscriber) =>
-    setSubscribers((currentSubscribers) => [
-      newSubscriber,
-      ...currentSubscribers,
-    ]);
-
-  const deleteSubscriber = (indexToDelete) =>
-    setSubscribers((currentSubscribers) =>
-      currentSubscribers.filter((post, index) => index !== indexToDelete)
-    );
-
-  return (
-    <>
-      <SubscriberForm createSubscriber={createSubscriber} />
-      <SubscriberList
-        subscribers={subscribers}
-        deleteSubscriber={deleteSubscriber}
-      />
-    </>
-  );
+  return <SubscriberForm />;
 }
 
 export default App;
