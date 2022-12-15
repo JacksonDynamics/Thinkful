2103e9981cf6b67c38ee0b0828bf82cad23aa3c9
studmanj <bjackso3@hotmail.com>
2022-12-14 19:15:17 -0700

    Submit Handlers

--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -15,23 +15,57 @@ function ProfileEdit() {
       loadUsers();
     }, []); // Passing [] so that it only runs the effect once
   
+    const changeHandler = event => {
+        setUser({ ...user, [event.target.name]: event.target.value })
+      }
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${user.id}`,
+          {
+            method: "PUT",
+            body: JSON.stringify(user),
+            headers: {
+                'Content-type':'application/json;charset=UTF-8'
+            },
+
+          }
+        );
+        const savedData = await response.json();
+        console.log("Saved user!", savedData);
+      };
+
+
     if (user.id) {
       // `user.id` is truthy after the API call returns
       return (
-        <form name="profileEdit">
+        <form name="profileEdit" onSubmit={submitHandler}>
           <div>
-            <label htmlFor="username">User Name:</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              value={user.username}
-            />
+            <label htmlFor="username">User Name:
+                <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    required={true}
+                    value={user.username}
+                    onChange={changeHandler}
+                />
+            </label>
           </div>
           <div>
-            <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" value={user.email} />
+            <label htmlFor="email">Email:
+                <input 
+                    id="email" 
+                    name="email" 
+                    type="email" 
+                    required={true}
+                    value={user.email}
+                    onChange={changeHandler} 
+                />
+            </label>
           </div>
+          <button type="submit">Submit</button>
         </form>
       );
     }
