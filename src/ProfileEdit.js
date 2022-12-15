9e3301f961eb6bfcb63caf0361efbbfeddd33ded
studmanj <bjackso3@hotmail.com>
2022-12-14 19:40:51 -0700

    Multiple Effect Hooks

--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -9,63 +9,65 @@ function ProfileEdit() {
           "https://jsonplaceholder.typicode.com/users/1"
         );
         const userFromAPI = await response.json();
-        console.log(userFromAPI)
         setUser(userFromAPI);
       }
       loadUsers();
     }, []); // Passing [] so that it only runs the effect once
   
-    const changeHandler = event => {
-        setUser({ ...user, [event.target.name]: event.target.value })
+    useEffect(() => {
+      if (user.username) {
+        document.title = `${user.username} : Edit Profile`;
+      } else {
+        document.title = "Edit Profile";
       }
-
+    }, [user]); // Rerun this effect when the user changes
+  
+    const changeHandler = (event) => {
+      setUser({ ...user, [event.target.name]: event.target.value });
+    };
+  
     const submitHandler = async (event) => {
-        event.preventDefault();
-        const response = await fetch(
-          `https://jsonplaceholder.typicode.com/users/${user.id}`,
-          {
-            method: "PUT",
-            body: JSON.stringify(user),
-            headers: {
-                'Content-type':'application/json;charset=UTF-8'
-            },
-
-          }
-        );
-        const savedData = await response.json();
-        console.log("Saved user!", savedData);
-      };
-
-
+      event.preventDefault();
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${user.id}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(user),
+          headers: {
+            'Content-type':'application/json;charset=UTF-8'
+          },
+        }
+      );
+      const savedData = await response.json();
+      console.log("Saved user!", savedData);
+    };
+  
     if (user.id) {
-      // `user.id` is truthy after the API call returns
       return (
         <form name="profileEdit" onSubmit={submitHandler}>
           <div>
-            <label htmlFor="username">User Name:
-                <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    required={true}
-                    value={user.username}
-                    onChange={changeHandler}
-                />
-            </label>
+            <label htmlFor="username">User Name:</label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              required={true}
+              value={user.username}
+              onChange={changeHandler}
+            />
           </div>
           <div>
-            <label htmlFor="email">Email:
-                <input 
-                    id="email" 
-                    name="email" 
-                    type="email" 
-                    required={true}
-                    value={user.email}
-                    onChange={changeHandler} 
-                />
-            </label>
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required={true}
+              value={user.email}
+              onChange={changeHandler}
+            />
           </div>
-          <button type="submit">Submit</button>
+          <button type="submit">Save</button>
         </form>
       );
     }
