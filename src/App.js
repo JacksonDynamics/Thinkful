9a761c964478f35fa393676020c14a9688f58574
studmanj <bjackso3@hotmail.com>
2022-12-16 18:35:57 -0700

    prettier

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
 
   return (
     <>
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NoMatch />} />
-    </Routes>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </>
   );
 }
